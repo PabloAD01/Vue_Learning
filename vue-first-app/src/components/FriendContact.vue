47ca9b79e420ead1177295f286eb1c1a4956d96e
Pablo Araya Diaz <piarayad@gmail.com>
2024-02-23 20:43:22 -0300

    add Vue-first-app

---
+++ vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel",
+        phone: "0123 45678 90",
+        email: "ZsXpY@example.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>

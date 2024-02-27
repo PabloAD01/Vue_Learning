e503744d75f7ea17a5c7600f1b7de84c70fc506f
Pablo Araya Diaz <piarayad@gmail.com>
2024-02-27 20:48:02 -0300

    feat: add VueProblem file + prove & inject

---
+++ VueProblem/src/components/KnowledgeGrid.vue
@@ -0,0 +1,32 @@
+<!-- <template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element>
+  </ul>
+</template> -->
+
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  inject: ['topics'],
+  /* props: ['topics'],
+  emits: ['select-topic'] */
+};
+</script>

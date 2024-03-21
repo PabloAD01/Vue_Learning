8aec2d2762bb6765848b3d794ff823f583cc6475
Pablo Araya Diaz <piarayad@gmail.com>
2024-03-20 23:56:31 -0300

    feat: add nested routing, redirect & catch routes, passing params as props

---
+++ VueRouting/src/components/nav/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>
+    The page you are looking for could not be found.
+    <router-link to="/teams">Go to Teams</router-link>
+  </h2>
+</template>
+
+<script>
+export default {};
+</script>

b6bfc8f796abd5a9fde24ffaa3f5df82054ac384
Pablo Araya Diaz <piarayad@gmail.com>
2024-03-29 20:14:42 -0300

    feat: add actions

--- VueX/src/App.vue
+++ VueX/src/App.vue
@@ -23,9 +23,9 @@ export default {
 
   methods: {
     addOne() {
-      /*       this.$store.commit('increase', { value: 10 });
+      /*       this.$store.dispatch('increase', { value: 10 });
        */
-      this.$store.commit({
+      this.$store.dispatch({
         type: 'increase',
         value: 10,
       });

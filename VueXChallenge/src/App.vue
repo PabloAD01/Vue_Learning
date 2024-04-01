65ed4482454db0c1bae44b9ba35ffe37195f84f9
Pablo Araya Diaz <piarayad@gmail.com>
2024-04-01 20:34:42 -0300

    feat: add VueX Page challenge

---
+++ VueXChallenge/src/App.vue
@@ -0,0 +1,48 @@
+<!-- El objetivo de este problema era implementar la lógica de Vuex en este proyecto.
+La lógica y la data se controlaban en los mismos componentes, lo que lo hacía
+ver mas engorroso.
+Al aplicar la lógica de Vuex en este proyecto, se logró hacer el proyecto
+mas legible y mantenible. -->
+
+<template>
+  <the-header></the-header>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+
+  methods: {
+    login() {
+      this.isLoggedIn = true;
+    },
+    logout() {
+      this.isLoggedIn = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>

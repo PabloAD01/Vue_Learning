5f349738964fe1f57ca221ba09f75af8f6903841
Pablo Araya Diaz <piarayad@gmail.com>
2024-04-04 21:44:13 -0300

    feat: add CoachItem file

--- VueWebApp/src/pages/coaches/CoachesList.vue
+++ VueWebApp/src/pages/coaches/CoachesList.vue
@@ -6,16 +6,26 @@
       <router-link to="/register">Register as a Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coaches in filteredCoaches" :key="coaches.id">
-        {{ coaches.firstName }} {{ coaches.lastName }}
-      </li>
+      <coach-item
+        v-for="coaches in filteredCoaches"
+        :key="coaches.id"
+        :id="coaches.id"
+        :first-name="coaches.firstName"
+        :last-name="coaches.lastName"
+        :rate="coaches.hourlyRate"
+        :areas="coaches.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -26,3 +36,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>

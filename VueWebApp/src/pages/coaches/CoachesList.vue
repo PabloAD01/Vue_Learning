aa6f50c6e53aea1a42bb800b6cb7d083a6c09e1c
Pablo Araya Diaz <piarayad@gmail.com>
2024-04-06 20:27:07 -0300

    feat: add UI folder + changes in CoachItem & CoachesList

--- VueWebApp/src/pages/coaches/CoachesList.vue
+++ VueWebApp/src/pages/coaches/CoachesList.vue
@@ -1,22 +1,24 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coaches in filteredCoaches"
-        :key="coaches.id"
-        :id="coaches.id"
-        :first-name="coaches.firstName"
-        :last-name="coaches.lastName"
-        :rate="coaches.hourlyRate"
-        :areas="coaches.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as a Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coaches in filteredCoaches"
+          :key="coaches.id"
+          :id="coaches.id"
+          :first-name="coaches.firstName"
+          :last-name="coaches.lastName"
+          :rate="coaches.hourlyRate"
+          :areas="coaches.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 

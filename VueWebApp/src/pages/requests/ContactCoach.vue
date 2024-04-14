7658903fadbcd6ed6986e64aec790ceff829dfc1
Pablo Araya Diaz <piarayad@gmail.com>
2024-04-13 21:31:18 -0300

    feat: add actions and mutations to requests folder

--- VueWebApp/src/pages/requests/ContactCoach.vue
+++ VueWebApp/src/pages/requests/ContactCoach.vue
@@ -37,12 +37,18 @@ export default {
 
       if (
         this.email === '' ||
-        this.email.includes('@') ||
+        !this.email.includes('@') ||
         this.message === ''
       ) {
         this.formIsValid = false;
         return;
       }
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.params.id,
+      });
+      this.$router.replace('/coaches');
     },
   },
 };
--- VueWebApp/src/store/index.js
+++ VueWebApp/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 import coachesModules from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModules,
+    requests: requestsModule,
   },
   state() {
     return {

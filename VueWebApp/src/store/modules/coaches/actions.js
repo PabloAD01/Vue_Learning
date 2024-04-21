27ef96bc8019f41f204c0663f25a749549d0fd1a
Pablo Araya Diaz <piarayad@gmail.com>
2024-04-20 22:00:34 -0300

    feat: add database interaction

--- VueWebApp/src/store/modules/coaches/actions.js
+++ VueWebApp/src/store/modules/coaches/actions.js
@@ -1,13 +1,28 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       description: payload.desc,
       hourlyRate: payload.rate,
       areas: payload.areas,
     };
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(
+      `${process.env.DATABASE_URL}/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    /*     const data = await response.json();
+     */
+    if (!response.ok) {
+      console.log(response);
+    }
+
+    context.commit('registerCoach', { ...coachData, id: userId });
   },
 };

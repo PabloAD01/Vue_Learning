d2da9cd6b2a317460d2bd58b4ff2a67e4c24c214
Pablo Araya Diaz <piarayad@gmail.com>
2024-04-22 21:36:37 -0300

    feat: add load coaches from database

--- VueWebApp/src/pages/coaches/CoachesList.vue
+++ VueWebApp/src/pages/coaches/CoachesList.vue
@@ -3,7 +3,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register as a Coach</base-button
         >
@@ -69,9 +69,17 @@ export default {
       return this.$store.getters['coaches/hasCoaches'];
     },
   },
+  created() {
+    this.loadCoaches();
+  },
+
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     },
   },
 };
--- VueWebApp/src/store/modules/coaches/actions.js
+++ VueWebApp/src/store/modules/coaches/actions.js
@@ -10,7 +10,7 @@ export default {
     };
 
     const response = await fetch(
-      `${process.env.DATABASE_URL}/coaches/${userId}.json`,
+      `https://vue-https-c5f76-default-rtdb.firebaseio.com/coaches/${userId}.json`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
@@ -24,5 +24,33 @@ export default {
     }
 
     context.commit('registerCoach', { ...coachData, id: userId });
+  },
+
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-https-c5f76-default-rtdb.firebaseio.com/coaches.json`
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.log(response);
+    }
+
+    const coaches = [];
+
+    for (const key in data) {
+      const coach = {
+        id: key,
+        firstName: data[key].firstName,
+        lastName: data[key].lastName,
+        description: data[key].description,
+        hourlyRate: data[key].hourlyRate,
+        areas: data[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   },
 };

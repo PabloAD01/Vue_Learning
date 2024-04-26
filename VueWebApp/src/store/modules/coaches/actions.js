e63ba3302b894d62ab74cf6967c38a953f1b979c
Pablo Araya Diaz <piarayad@gmail.com>
2024-04-25 21:44:09 -0300

    feat: caching data

--- VueWebApp/src/store/modules/coaches/actions.js
+++ VueWebApp/src/store/modules/coaches/actions.js
@@ -26,7 +26,11 @@ export default {
     context.commit('registerCoach', { ...coachData, id: userId });
   },
 
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
       `https://vue-https-c5f76-default-rtdb.firebaseio.com/coaches.json`
     );
@@ -53,5 +57,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   },
 };
